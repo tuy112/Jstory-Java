742013e4fd847249a0e91a518edf8cd53752af0e
Kim Jae Hyeok <master_catch@naver.com>
2024-08-12 22:51:01 +0900

    [서브] profile, project 화면 신규 생성

---
+++ src/main/resources/static/js/sub.js
@@ -0,0 +1,8 @@
+// 글쓰기 버튼 모달 띄우기
+function openModal() {
+    document.getElementById('modal').style.display = 'flex';
+}
+// 글쓰기 버튼 모달 닫기
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
