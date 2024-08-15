e2985d8061b972d28ea0b8dfe39fd0d128cb81c5
Kim Jae Hyeok <master_catch@naver.com>
2024-08-16 02:07:41 +0900

    [프로젝트] 각 controller 파일 경로 변경

--- src/main/java/com/jstory/mountain/ClimbMountController.java
+++ src/main/java/com/jstory/mountain/ClimbMountController.java
@@ -1,4 +1,4 @@
-package com.mountain;
+package com.jstory.mountain;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/jstory/todo/JTodoController.java
+++ src/main/java/com/jstory/todo/JTodoController.java
@@ -1,4 +1,4 @@
-package com.todo;
+package com.jstory.todo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
